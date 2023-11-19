d9cdf1b63b8ddb36cc41735df30fca27ef4fe0ad
Claiton Amaral <dr_amaral@msn.com>
2023-11-19 15:44:58 -0300

    config cors

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const secretKey = '${process.env.SECRET_KEY}';
 
 exports.generateToken = (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
     
     const user = { id: process.env.ID, username: process.env.USERNAME };
 
