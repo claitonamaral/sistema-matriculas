061222ff3ffd3acc439b06cda7dc22782adfb424
Claiton Amaral <dr_amaral@msn.com>
2023-11-19 15:11:42 -0300

    function allow-origin

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 const express = require('express');
-const cors =  require('cors');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
-const corsOptions = {
-    origin: 'https://api-matricula.onrender.com', 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
-    optionsSuccessStatus: 200
-  };
-app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Origin', 'Content-Type');
+    next();
+})
+
 app.use('/api/v1', authRoutes);
 
 app.listen(PORT, () => {
