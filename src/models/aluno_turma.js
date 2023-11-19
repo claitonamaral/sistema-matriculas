74e7f0a93531c2ac5dc36feca514138abcf6c3a9
arthurExner <arthur.exner1@gmail.com>
2023-11-19 00:57:40 -0300

    novas regras de negocio para controle de turmas e alunos

--- src/models/aluno_turma.js
+++ src/models/aluno_turma.js
@@ -13,12 +13,18 @@ class AlunoTurma {
     db.all('SELECT id_aluno, id_turma FROM aluno_turma WHERE id=?', id,  callback);
   }
   
-  /*static update(id, { id_turma, id_aluno }, callback) {
-    db.run('UPDATE aluno_turma SET id_turma=?, id_aluno=? WHERE id=?', [id, id_turma, id_aluno ], callback);
-  }*/
+  static update(id, { id_aluno, id_turma }, callback) {
+    db.run('UPDATE aluno_turma SET id_aluno=?, id_turma=? WHERE id=?', [id_aluno, id_turma, id], callback);
+  }
 
+  //quantos alunos por turma
   static getCountTurma(id_turma, callback) {
     db.all('SELECT COUNT(*) AS count FROM aluno_turma WHERE id_turma=?', id_turma,  callback);
+  }
+
+  //se ja tem esse aluno na turma
+  static getCountAlunoTurma({id_aluno, id_turma}, callback) {
+    db.all('SELECT COUNT(*) AS count FROM aluno_turma WHERE id_aluno=? AND id_turma=?', [id_aluno, id_turma],  callback);
   }
 
   static deleteAlunoFromTurma(id, callback) {
--- src/models/turma.js
+++ src/models/turma.js
@@ -1,8 +1,8 @@
 const db = require('../db');
 
 class Turma {
-  static create({ id_professor, id_disciplina }, callback) {
-    db.run('INSERT INTO turmas (id_professor, id_disciplina) VALUES (?, ?)', [id_professor, id_disciplina], callback);
+  static create({ id_professor, id_disciplina, id_turno, id_diaSemana }, callback) {
+    db.run('INSERT INTO turmas (id_professor, id_disciplina, id_turno, id_diaSemana) VALUES (?, ?, ?, ?)', [id_professor, id_disciplina, id_turno, id_diaSemana], callback);
   }
 
   static getAll(callback) {
@@ -13,12 +13,28 @@ class Turma {
     db.all('SELECT * FROM turmas WHERE id=?', id, callback);
   }
 
-  static getCountProfessor(id_professor, callback) {
-    db.all('SELECT COUNT(*) AS count FROM turmas WHERE id_professor=?', id_professor, callback);
+  //quantas turmas o professor pode se matricular
+  static getCountProfessor({id_professor}, callback) {
+    db.all('SELECT COUNT(*) AS count FROM turmas WHERE id_professor=?', [id_professor], callback);
   }
 
-  static update(id, {id_disciplina, id_professor}, callback) {
-    db.run('UPDATE turmas SET id_professor=?, id_disciplina=? WHERE id=?', [id, id_disciplina, id_professor], callback);
+  //quantas turmas por turno em um dia 
+  static getCountTurmaTurno({ id_turno, id_diaSemana }, callback) {
+    db.all('SELECT COUNT(*) AS count FROM turmas WHERE id_turno=? AND id_diaSemana=?', [id_turno, id_diaSemana], callback);
+  }
+
+  //se ja tem esse professor nessa disciplina nesse turno e dia
+  static getCountProfDiscTurmaTurno({ id_disciplina, id_turno, id_diaSemana }, callback) {
+    db.all('SELECT COUNT(*) AS count FROM turmas WHERE id_disciplina=? AND id_turno=? AND id_diaSemana=?', [id_disciplina, id_turno, id_diaSemana], callback);
+  }
+
+  //quantas turmas serão ofertadas por disciplina
+  static getCountDisciplina( id_disciplina , callback) {
+    db.all('SELECT COUNT(*) AS count FROM turmas WHERE id_disciplina=?', [id_disciplina], callback);
+  }
+
+  static update(id, {id_professor, id_disciplina, id_turno, id_diaSemana}, callback) {
+    db.run('UPDATE turmas SET id_professor=?, id_disciplina=?, id_turno=?, id_diaSemana=? WHERE id=?', [id_professor, id_disciplina, id_turno, id_diaSemana, id], callback);
   }
 
   static delete(id, callback) {
