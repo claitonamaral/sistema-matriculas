1e4e497e6d1320fa5d312bef4dc5f8c46764ac80
Claiton Amaral <dr_amaral@msn.com>
2023-11-19 13:51:45 -0300

    config cors no authRoutes

--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const cors = require('cors')
 const AlunoController = require('../controllers/alunoController');
 
 const router = express.Router();
 
-var corsOptions = {
-    origin: 'https://api-matricula.onrender.com',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-router.post('/', cors(corsOptions), AlunoController.createAluno);
-router.get('/', cors(corsOptions), AlunoController.getAllAlunos);
-router.get('/:id', cors(corsOptions), AlunoController.getAluno);
-router.put('/:id', cors(corsOptions), AlunoController.updateAluno);
-router.delete('/:id', cors(corsOptions), AlunoController.deleteAluno);
+router.post('/', AlunoController.createAluno);
+router.get('/', AlunoController.getAllAlunos);
+router.get('/:id', AlunoController.getAluno);
+router.put('/:id', AlunoController.updateAluno);
+router.delete('/:id', AlunoController.deleteAluno);
 
 module.exports = router;
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,17 @@
 const { Router } = require('express');
+const cors = require('cors');
 const authRouter = Router();
+
+const corsOptions = {
+    origin: 'http://your-allowed-origin.com', // Especifique a origem permitida
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+  };
 
 const authController = require('../controllers/authController');
 const alunoRoutes = require('../routes/alunoRoutes');
 const swaggerRouter = require('./swaggerRoutes');
 
-authRouter.post('/generate-token', authController.generateToken);
+authRouter.post('/generate-token', cors(corsOptions), authController.generateToken);
 authRouter.use('/swagger-ui', swaggerRouter);
 authRouter.use("/aluno", alunoRoutes);
 
