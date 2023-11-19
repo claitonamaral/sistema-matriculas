b048b375dc6fbe0b25349a74fde63be664106d22
Claiton Amaral <dr_amaral@msn.com>
2023-11-19 01:55:25 -0300

    config o cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/v1', authRoutes);
 
