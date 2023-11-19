bc7db1890d712778434c7de37331485f016c1457
Claiton Amaral <dr_amaral@msn.com>
2023-11-19 10:39:37 -0300

    config o cors router

--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const AlunoController = require('../controllers/alunoController');
 const authenticateToken = require('../middleware/authenticateToken'); 
 
 const router = express.Router();
 
 router.post('/', AlunoController.createAluno);
-router.get('/', AlunoController.getAllAlunos);
+router.get('/', cors(), AlunoController.getAllAlunos);
 router.get('/:id', AlunoController.getAluno);
 router.put('/:id', AlunoController.updateAluno);
 router.delete('/:id', AlunoController.deleteAluno);
