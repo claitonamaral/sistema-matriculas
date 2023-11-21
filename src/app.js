dd5673a60b674f5e5c9ae678931f3e667c657e25
Claiton Amaral <dr_amaral@msn.com>
2023-11-20 21:46:49 -0300

    CORS

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
-
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+};
 
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
 app.use('/api/v1', authRoutes);
 
 app.listen(PORT, () => {
