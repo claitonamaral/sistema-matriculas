ab12e0f082dc00c424c80c52a093a2cd518f8858
Claiton Amaral <dr_amaral@msn.com>
2023-11-20 23:27:58 -0300

    refactoring swagger

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./swagger');
-const routes = require('./routes/authRoutes');
+const swaggerSpec = require('./swaggerConfig');
+const exemploRoutes = require('./routes/exemploRoutes');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Configurar o middleware CORS
 app.use(cors());
 
-// Rota principal
-app.get('/', (req, res) => {
-    res.send('Bem-vindo à API!');
+// Middleware de exemplo
+app.use((req, res, next) => {
+  console.log('Middleware de exemplo');
+  next();
 });
 
-// Rota de exemplo
+// Rota principal
+app.get('/', (req, res) => {
+  res.send('Bem-vindo à API!');
+});
+
+// Rotas de exemplo
 app.use('/api/v1', routes);
 
 // Configurar Swagger
@@ -23,5 +29,5 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Iniciar o servidor
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
